d49e87684ca1112485bc19a3eda6cd579e84ff1e
Jasurbek Normurodov <jasurbek.normuradov@gmail.com>
2022-04-20 14:50:42 +0500

    add a context

---
+++ useFocusedContext.js
@@ -0,0 +1,6 @@
+import { useContext, createContext } from "solid-js";
+import { ROOT_FOCUS_KEY } from "./SpatialNavigation";
+
+export const FocusContext = createContext(ROOT_FOCUS_KEY);
+
+export const useFocusedContext = () => useContext(FocusContext);
