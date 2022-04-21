b0eb4522dc8fc4b3165be144783c9eb305fd0d9c
normuradoff <normuradoff1@gmail.com>
2022-04-21 13:56:46 +0500

    add demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,41 @@
+import { createEffect, onMount } from 'solid-js';
+import {
+  init,
+  useFocusable,
+  FocusContext,
+} from './packages/spatial-navigation';
+
+init({ debug: false });
+
+function Button() {
+  const { ref, setRef, focused, focusKey, focusSelf, setFocus } =
+    useFocusable();
+
+  createEffect(() => console.log(focused()));
+
+  return (
+    <button
+      onClick={() => setFocus(focusKey)}
+      ref={(el) => setRef(el)}
+      className={focused() ? 'button-focused' : 'button'}
+    >
+      Press me
+    </button>
+  );
+}
+
 function App() {
-  return <div>Solid App</div>;
+  const { setRef, focusKey } = useFocusable({
+    focusKey: 'app',
+  });
+  return (
+    <FocusContext.Provider value={focusKey}>
+      <>
+        <div ref={(el) => setRef(el)}>Solid App</div>
+        <Button />
+      </>
+    </FocusContext.Provider>
+  );
 }
 
 export default App;
