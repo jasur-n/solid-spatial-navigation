ed4ed8ecdbfd21f7c4651047de779bba90def73d
Jasurbek Normurodov <jasurbek.normuradov@gmail.com>
2022-04-22 14:18:36 +0500

    minor fixes

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
 
-import App from "./App";
+// import App from "./App";
 import NorginApp from "./NorginApp";
 
 render(() => <NorginApp />, document.getElementById("root"));
