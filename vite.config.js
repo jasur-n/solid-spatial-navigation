cc0d025fdab5896ec799b04f22c308494721d66f
Jasurbek Normurodov <jasurbek.normuradov@gmail.com>
2022-04-30 13:11:16 +0500

    add path aliases to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import path from 'path'
 
 export default defineConfig({
   plugins: [solidPlugin()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
   build: {
     target: 'esnext',
     polyfillDynamicImport: false,
